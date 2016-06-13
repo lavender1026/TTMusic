9d97791e46a44618e7cc5de9525d9f1534831cfc
DirkChen <10073970@qq.com>
2016-06-13 14:03:13 +0800

    搭建界面竖直方向搞定 开始水平方向

---
+++ TTMusic/TTMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTMusic
+//
+//  Created by admin on 16/6/13.
+//  Copyright © 2016年 DirkChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
